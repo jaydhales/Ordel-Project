717fac6ab7f17e99f3d7458da1e15d9c6459bfd7
jaydhales <jaydhales@gmail.com>
2021-07-18 15:50:31 +0100

    commit

--- Exercise/index.js
+++ Exercise/index.js
@@ -1,2 +1,22 @@
-var hamburgerBtn = document.querySelector('.hmb');
+var startBtns = document.querySelectorAll('.start');
+var caret = document.querySelectorAll('.caret');
 
+function eventClick (variable, func) {
+    for (let btn = 0; btn < variable.length; btn++) {
+        variable[btn].addEventListener('click', func );
+    }
+}
+
+function logIn () {
+    startBtns[0].textContent = 'J';
+    startBtns[0].style.borderRadius = '50%';
+    startBtns[0].style.padding = '8px 18.85px';
+    startBtns[1].textContent = 'Start Shopping';
+}
+
+function cDown () {
+    console.log(this);
+}
+
+eventClick( startBtns , logIn);
+eventClick( caret, cDown);
