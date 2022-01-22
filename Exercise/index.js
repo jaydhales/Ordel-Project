8d630fe5785ed10b0e01273bebd9d97c70663f09
james <jaydhales@gmail.com>
2022-01-22 14:09:41 +0100

    final commit

--- Exercise/index.js
+++ Exercise/index.js
@@ -1,22 +1,51 @@
-var startBtns = document.querySelectorAll('.start');
-var caret = document.querySelectorAll('.caret');
+const startBtns = document.querySelectorAll('.start');
+const caret = document.querySelectorAll('.caret');
+const navBtn = document.querySelectorAll('.hmb');
+const navBar = document.querySelector('#navbar');
+const copyright = document.querySelector('small');
 
-function eventClick (variable, func) {
-    for (let btn = 0; btn < variable.length; btn++) {
-        variable[btn].addEventListener('click', func );
-    }
+function eventL(variable, event, func) {
+  for (let btn = 0; btn < variable.length; btn++) {
+    variable[btn].addEventListener(event, func);
+  }
 }
 
-function logIn () {
-    startBtns[0].textContent = 'J';
-    startBtns[0].style.borderRadius = '50%';
-    startBtns[0].style.padding = '8px 18.85px';
-    startBtns[1].textContent = 'Start Shopping';
+function logIn() {
+  startBtns[0].textContent = 'J';
+  startBtns[0].style.borderRadius = '50%';
+  startBtns[1].textContent = 'Start Shopping';
 }
 
-function cDown () {
-    console.log(this);
+function cDown(e) {
+  e.preventDefault();
+  let caretBtn;
+
+  if (e.target.tagName === 'IMG') {
+    caretBtn = e.target.parentNode;
+  } else {
+    caretBtn = e.target;
+  }
+
+  caretBtn.classList.toggle('active');
+
+  caretBtn.parentNode.parentNode.classList.toggle('active');
 }
 
-eventClick( startBtns , logIn);
-eventClick( caret, cDown);
+function changeNav() {
+  navBar.classList.toggle('active');
+}
+
+function eventClick(variable, func) {
+  eventL(variable, 'click', func);
+}
+
+eventClick(startBtns, logIn);
+eventClick(caret, cDown);
+eventClick(navBtn, changeNav);
+eventL(window, 'resize', (e) => {
+  if (e.target.innerWidth === 700) {
+    navBar.classList.remove('active');
+  }
+});
+
+copyright.innerHTML = `&copy; ${new Date().getFullYear()} All Rights Reserved`;
